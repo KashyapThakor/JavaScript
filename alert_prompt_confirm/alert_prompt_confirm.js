2e3dc2f3322790a9798291ea4b4f32de2a961b40
KashyapThakor <150549633+KashyapThakor@users.noreply.github.com>
2024-09-13 03:20:44 +0530

    Add files via upload

---
+++ alert_prompt_confirm/alert_prompt_confirm.js
@@ -0,0 +1,13 @@
+//alert() --> used to invoke a mini window with a message
+//prompt() --> used to take user input as string
+//confirm() --> shows a message and waits for the user reply . 
+//              If user say Ok it returns True 
+//              If user say cancel it returns False
+
+alert("Enter the value of a!");
+
+let a = prompt("ENter a here","123");
+console.log(a)
+
+let write = confirm("Do you want to write")
+console.log(write)
